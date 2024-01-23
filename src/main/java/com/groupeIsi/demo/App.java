5a3872bcdc040b958c0679c9aa4338f35aea7e81
ThiamAwa <awa96362@gmail.com>
2024-01-23 11:47:19 +0000

    ajout projet

---
+++ src/main/java/com/groupeIsi/demo/App.java
@@ -0,0 +1,13 @@
+package com.groupeIsi.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
